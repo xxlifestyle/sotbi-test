c8db58c3a041d6971ffc65cbe32a6ed7703e5a67
Vladislav <32987720+xxlifestyle@users.noreply.github.com>
2021-10-20 13:40:23 +0300

    +logo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+
+  <logo></logo>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Logo from './components/Logo.vue';
+
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Logo
+    
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,19 @@
+<template lang="">
+Логотип Яндекс Такси:
+    <div class="logo">
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style >
+.logo{
+    width: 100px;
+    height: 100px;
+    border-radius: 25px;
+    background-image: linear-gradient(to top, transparent 0%, transparent 50%, #ffc501 50%, #ffc501 100%),linear-gradient(to right, #eef0f0 50%, black 50%)
+}
+    
+</style>
